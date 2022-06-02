16dfeb62d6c322aeefe6158c29beb73e64be5378
shweta127 <shwetamulewar2593@gmail.com>
2022-06-03 00:30:03 +0530

    demo

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,13 +3,16 @@ const externalModule = require('../logger/logger.js')
 const externalModule1 =require('../util/helper.js')
 const externalModule2 =require('../validator/formatter.js')
 const router = express.Router()
-router.get('/test-me', function (req, res) {
-    externalModule.w();
-   externalModule1.help1();
-    externalModule2.trim();
-   externalModule2.lowerCase1();
-    externalModule2.upperCase1();
-    res.send('My first ever api!')
+router.get('/hello, function (req, res) {
+    externalModule.w(;
+        externalModule1.help1();
+        externalModule2.trim();
+        externalModule2.lowerCase1();
+        externalModule2.upperCase1();
+        res.send("my first ever api")
+        
+        
+        )
 });
 
 
