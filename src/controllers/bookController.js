be9e72959dd49629686c6a3b9e522e5f0cb0923f
shweta127 <shwetamulewar2593@gmail.com>
2022-06-07 23:44:15 +0530

    authorbook

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,47 +1,41 @@
-const { count } = require("console")
-const BookModel= require("../models/bookModel")
 
-const createBook= async function (req, res) {
+const BookModel= require("../models/bookModel")
+const AuthorModel= require("../models/authorModel")
+const bookModel = require("../models/bookModel")
+
+let createAuthor= async function (req, res){
+    let data= req.body
+
+    let savedData= await AuthorModel.create(data)
+    res.send({msg: savedData})
+}
+let createBook= async function (req, res){
     let data= req.body
 
     let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find( {authorName : "HO" } )
-    console.log(allBooks)
-    if (allBooks.length > 0 )  res.send({msg: allBooks, condition: true})
-    else res.send({msg: "No books found" , condition: false})
+let getBooksbyChetanBhagat=async(req, res)=>{
+    let data=await AuthorModel.find({author_name:"Chetan Bhagat"}).select("author_id")
+    let bookData =await BookModel.find({author_id:data[0].author_id})
+    res.send({msg:bookData})
 }
 
+let authorofBook=async(req, res)=>{
+    let data=await BookModel.findOneAndUpdate({name:"Two states"},{$set:{prices:100}},{new:true})
+    let authorData =await AuthorModel.find({author_id:data.author_id}).select("author_Name")
+    let price=data.prices
+    res.send({msg:authorData,price})
+}
+let bookBetween50_100=async(req, res)=>{
 
-const updateBooks= async function (req, res) {
-    let data = req.body // {sales: "1200"}
-    // let allBooks= await BookModel.updateMany( 
-    //     { author: "SK"} , //condition
-    //     { $set: data } //update in data
-    //  )
-    let allBooks= await BookModel.findOneAndUpdate( 
-        { authorName: "ABC"} , //condition
-        { $set: data }, //update in data
-        { new: true , upsert: true} ,// new: true - will give you back the updated document // Upsert: it finds and updates the document but if the doc is not found(i.e it does not exist) then it creates a new document i.e UPdate Or inSERT  
-     )
-     
-     res.send( { msg: allBooks})
 }
 
-const deleteBooks= async function (req, res) {
-    // let data = req.body 
-    let allBooks= await BookModel.updateMany( 
-        { authorName: "FI"} , //condition
-        { $set: {isDeleted: true} }, //update in data
-        { new: true } ,
-     )
-     
-     res.send( { msg: allBooks})
-}
-
+module.exports. createAuthor=  createAuthor
+module.exports.createBook =  createBook
+module.exports. getBooksbyChetanBhagat=  getBooksbyChetanBhagat
+module.exports. authorofBook= authorofBook
 
 
 
@@ -51,9 +45,3 @@ const deleteBooks= async function (req, res) {
 // UPDATE
 // DELETE
 
-
-
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.updateBooks= updateBooks
-module.exports.deleteBooks= deleteBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -12,30 +12,12 @@ router.post("/createUser", UserController.createUser  )
 
 router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+router.post("/createAuthor", BookController.createAuthor )
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/getBooksbyChetanBhagat", BookController.getBooksbyChetanBhagat)
 
-router.post("/updateBooks", BookController.updateBooks)
-router.post("/deleteBooks", BookController.deleteBooks)
+router.post("/authorofBook", BookController.authorofBook)
 
-//MOMENT JS
-const moment = require('moment');
-router.get("/dateManipulations", function (req, res) {
-    
-    // const today = moment();
-    // let x= today.add(10, "days")
 
-    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
-    // console.log(validOrNot)
-    
-    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
-    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
-
-    let x= dateB.diff(dateA, "days")
-    console.log(x)
-
-    res.send({ msg: "all good"})
-})
 
 module.exports = router;
